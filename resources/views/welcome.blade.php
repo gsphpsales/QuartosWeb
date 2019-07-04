34e197ac582b1f5d3b105f3e28fca18efb047fd5
Gilson Sales <gilson.sales21@hotmail.com>
2019-07-04 15:58:13 -0300

    Form test
    form to text reser.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -205,7 +205,31 @@
 
             <h3><i class="material-icons">rate_review</i>
             Resevar</h3> 
-            <div class="pg">         
+            <div class="pg">  
+            <form action="/QuartosWeb/public/res" method="POST">
+              <input type="hidden" name="_token" value="{{ csrf_token() }}">
+              <div class="form-group">
+                <label for="pwd">Nome completo</label>
+                <input type="text" class="form-control" name="name" id="name" >
+              </div>
+
+              <div class="form-group">
+                <label for="email">Email address:</label>
+                <input type="email" class="form-control" name="email" id="email">
+              </div>
+
+              <div class="form-group">
+                <label>Entrada</label>
+                <input type="date" name="inp" id="inp">
+                <label>Saída</label>
+                <input type="date" name="out" id="out">
+                  
+              </div>
+              
+              
+              <button type="submit" class="btn btn-default">Reservar</button>
+            </form>
+
           
           </div>    
           </div>
